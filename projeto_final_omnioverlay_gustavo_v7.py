2576fadb9d04b28fabd7d3c953b1e6e0e8622d24
Aluna Vocacao Maria Eduarda <dduda.rmello@gmail.com>
2026-09-21 10:25:00 -0300

    Update projeto_final_omnioverlay_gustavo_v7.py

--- projeto_final_omnioverlay_gustavo_v7.py
+++ projeto_final_omnioverlay_gustavo_v7.py
@@ -759,6 +759,8 @@ class DashboardFrame(ctk.CTkFrame):
             except Exception as e:
                 print(f"[ERRO] Falha ao carregar fundo de tela: {e}")
         self.lbl_fundo_bg.configure(image="")
+        
+    
 
     def carregar_perfil(self, perfil):
         self.lbl_titulo.configure(text=f"OmniOverlay - {perfil['nome']}")
@@ -1469,6 +1471,25 @@ class DashboardFrame(ctk.CTkFrame):
             self.controller.perfil_ativo["fundo_imagem"] = caminho
             AccountManager.salvar_dados(self.controller.dados_config)
             self.atualizar_fundo_tela(caminho)
+            
+    def atualizar_fundo_tela(self, caminho_img):
+        if caminho_img and os.path.exists(caminho_img):
+            try:
+                pil_img = Image.open(caminho_img)
+                pil_img = pil_img.resize((920, 800), Image.Resampling.LANCZOS)
+                
+                # Salvamos a referência para evitar que o Garbage Collector apague a imagem
+                self.imagem_fundo_atual = ctk.CTkImage(light_image=pil_img, dark_image=pil_img, size=(920, 800))
+                
+                self.lbl_fundo_bg.configure(image=self.imagem_fundo_atual, text="")
+                self.lbl_fundo_bg.place(x=0, y=0, relwidth=1, relheight=1)
+                self.lbl_fundo_bg.lower() # Mantém estritamente no fundo
+                return
+            except Exception as e:
+                print(f"[ERRO] Falha ao carregar fundo de tela: {e}")
+        
+        self.imagem_fundo_atual = None
+        self.lbl_fundo_bg.configure(image="", text="")
 
 
 class OmniOverlayApp(ctk.CTk):
